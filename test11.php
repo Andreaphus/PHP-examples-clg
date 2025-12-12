de14e78fa979d11335b1b7ff022b108224c7a404
Andreaphus <151429538+Andreaphus@users.noreply.github.com>
2025-12-12 15:29:37 +0530

    Add files via upload

---
+++ test11.php
@@ -0,0 +1,15 @@
+<?php
+for($i=0;$i<5;$i++)
+{
+print "$i<br>";
+}
+?>
+
+<?php
+$i=0;
+while($i<5)
+{
+print "$i<br>";
+$i++;
+}
+?>
---
+++ test12.php
@@ -0,0 +1,8 @@
+<?php
+$num=5;
+for($i=0;$i<$num;i++)
+{
+print $num;
+$num=$i*$num;
+}
+?>
