e144feefcc81ef697578adb173c4d3f2587670f7
Andreaphus <151429538+Andreaphus@users.noreply.github.com>
2025-12-20 15:27:06 +0530

    Add files via upload

---
+++ test22.php
@@ -0,0 +1,14 @@
+<html>
+<pre>
+<?php
+$myarray=array(
+				array("Ankit","Ram","Shayam"),
+				array("Delhi","Mumbai","Kanpur")
+			 );
+foreach($myarray as $name)
+{
+echo $name[0]." ".$name[1]." ".$name[2]."<br>";
+} 
+?>
+</pre>
+</html>
---
+++ test23.php
@@ -0,0 +1,15 @@
+<html>
+<pre>
+<?php
+$myarray=array(
+				array(1,2,3),
+				array(4,5,6),
+				array(7,8,9)
+			 );
+foreach($myarray as $name)
+{
+echo $name[0]." ".$name[1]." ".$name[2]."<br>";
+} 
+?>
+</pre>
+</html>
